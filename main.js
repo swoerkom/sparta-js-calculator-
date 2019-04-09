145db81bafa9c7cc6280bce4873cf397ff2de6b1
swoerkom <swoerkom@spartaglobal.com>
2019-04-09 09:26:12 +0100

    Fixed power of calculation

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
-var calculator_choice = prompt("What calculator do you want to use, (b)asic and (a)dvanced?");
-if (calculator_choice.toLowerCase() == "b") {
+var calculator_choice = prompt("What calculator do you want to use, basic (1) and advanced(2)?");
+if (calculator_choice.toLowerCase() == "1") {
   var firstNumber = parseInt(prompt("What is the first number to operate on?"));
   var secondNumber = parseInt(prompt("What is the second number to operate on?"));
   var operation = prompt("Do you want to * / - or + the numbers?")
@@ -15,7 +15,7 @@ if (calculator_choice.toLowerCase() == "b") {
     var answer = firstNumber - secondNumber;
   }
 }
-  else if (calculator_choice.toLowerCase() == "a") {
+  else if (calculator_choice.toLowerCase() == "2") {
     var number = parseInt(prompt("What is the number you want to operate on?"));
     var operation = prompt("Do you want to square root (1) or find the power of (2)?");
 
@@ -23,7 +23,9 @@ if (calculator_choice.toLowerCase() == "b") {
       var answer = Math.sqrt(number);
 
     } if (operation ==="2") {
-      var answer = Math.pow(number, number);
+      var powerOf = parseInt(prompt("To the power of?"));
+
+      var answer = Math.pow(number, powerOf);
     }
 }
 alert(answer);
